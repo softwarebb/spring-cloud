6572e2bd138f4ec2c556368d8374689a7b268eae
96234 <962347255@qq.com>
2021-07-18 21:22:39 +0800

    spring cloud zipkin 链路跟踪

--- cloud-zipkin-service-hi/src/main/java/com/example/tmy/config/AppConfig.java
+++ cloud-zipkin-service-hi/src/main/java/com/example/tmy/config/AppConfig.java
@@ -1,6 +1,6 @@
 package com.example.tmy.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import brave.sampler.Sampler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -14,8 +14,12 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
     @Bean
-    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Sampler getSample() {
+        return Sampler.ALWAYS_SAMPLE;
     }
 }
--- cloud-zipkin-service-miya/src/main/java/com/example/tmy/CloudZipkinServiceMiyaApplication.java
+++ cloud-zipkin-service-miya/src/main/java/com/example/tmy/CloudZipkinServiceMiyaApplication.java
@@ -1,7 +1,9 @@
 package com.example.tmy;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CloudZipkinServiceMiyaApplication {
@@ -9,5 +11,8 @@ public class CloudZipkinServiceMiyaApplication {
     public static void main(String[] args) {
         SpringApplication.run(CloudZipkinServiceMiyaApplication.class, args);
     }
-
+    @Bean
+    public Sampler getSample() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
 }
